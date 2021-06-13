e2b62f7b00932f13a3553cec52879696e37bc05d
Landari Safa <Landari.Safa@issatso.u-sousse.tn>
2021-06-13 18:04:15 +0100

    Delete Security project + OOnly one dbContext + add RealisateurId to Tache entity + add ChefProjetId To Projet entity

--- PlanPro.Business/Migrations/20210613160253_initDB.Designer.cs
+++ PlanPro.Business/Migrations/20210613160253_initDB.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlanPro.Business;
 
 namespace PlanPro.Business.Migrations
 {
     [DbContext(typeof(PlanProDbContext))]
-    partial class PlanProDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210613160253_initDB")]
+    partial class initDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PlanPro.Entities/ViewModels/Response.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanPro.Entities.ViewModels
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
